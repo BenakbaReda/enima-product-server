c3e3bcf240a469f1e4f5d4d9c7c387db79ad229a
GitRedaAmine <75283738+GitRedaAmine@users.noreply.github.com>
2022-02-21 08:00:02 +0100

    use Id , coorect bug

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const router = jsonServer.router('db.json', {foreignKeySuffix: 'Id'});
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+router.db._.id = 'Id';
+
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port,() => {
+ 
+  console.log('JSON Server is running , and listen to port :' + port)
+})
